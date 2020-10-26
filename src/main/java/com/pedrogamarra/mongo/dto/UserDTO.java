2f5da281df557bc646271dd7f0b2473b9541c3f0
Pedro Henrique Sobrinho Gamarra da Silva <Pedrohhouro@gmail.com>
2020-10-26 16:07:40 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/com/pedrogamarra/mongo/dto/UserDTO.java
@@ -0,0 +1,46 @@
+package com.pedrogamarra.mongo.dto;
+
+import java.io.Serializable;
+
+import com.pedrogamarra.mongo.domain.User;
+
+public class UserDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name, email;
+	
+	private UserDTO() {
+	}
+	
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
