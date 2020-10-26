d0921e0a819aba40c17762b2286f573c872d1727
Pedro Henrique Sobrinho Gamarra da Silva <Pedrohhouro@gmail.com>
2020-10-26 14:31:20 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/pedrogamarra/mongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrogamarra.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
